a36c766313e2e7d2773f1fd2357efc49ec988fce
Agustin Carbajal <magustincarbajal97@gmail.com>
2024-06-25 20:20:31 -0300

    [feature] postcards

--- app/src/main/java/com/example/medanospaper/ui/theme/Color.kt
+++ app/src/main/java/com/example/medanospaper/ui/theme/Color.kt
@@ -5,6 +5,10 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF123033)
+val Secondary = Color(0XFFE53F2C)
+val Terciary = Color(0XFF31878E)
+val Background = Color(0XFF266A70)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
